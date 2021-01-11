8b6fa31d9c237a08b6254dc78e5c00c94b76a5e8
sujpac <sujit.pack@gmail.com>
2021-01-10 17:14:42 -0800

    adding existing code

---
+++ summarize_v2.py
@@ -0,0 +1,21 @@
+from newspaper import Article
+
+# import nltk
+# pip install nltk
+# pip install newspaper3k
+
+url = 'https://www.cnn.com/2021/01/10/politics/paid-sick-leave-covid-benefit-expiration/index.html'
+article = Article(url)
+article.download()
+article.parse()
+article.nlp()
+'''
+useful_state = [
+    article.authors,
+    article.publish_date,
+    article.top_image,
+    article.keywords,
+    article.text,
+]
+'''
+print('Article Summary:\n', article.summary)
